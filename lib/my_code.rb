2a45c3e319bd15dc859f4a8a2eddfae2cf296421
Max M <maxmcfadden88@gmail.com>
2019-11-16 01:03:30 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,71 @@
-# My Code here....
+def map_to_negativize(array)
+  newarray = []
+  i = 0
+  
+  while i < array.length do 
+    newarray << array[i]*-1
+  i += 1
+end
+return newarray
+end 
+
+def map_to_no_change(array)
+  newarray = []
+  i = 0
+  
+  while i < array.length do 
+    newarray << array[i]
+    i += 1
+  end
+  return newarray
+end
+
+def map_to_double(array)
+    newarray = []
+  i = 0
+  
+  while i < array.length do 
+    newarray << array[i]*2
+  i += 1
+end
+return newarray
+end 
+
+def map_to_square(array)
+  newarray = []
+  i = 0
+  
+  while i < array.length do 
+    newarray << array[i]**2
+  i += 1
+end
+return newarray
+end 
+
+def reduce_to_total(array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < array.length do
+    new += array[i]
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_all_true(array)
+  i = 0
+  while i < array.length do
+    return false if !array[i]
+    i += 1
+  end
+  return true
+end
+
+def reduce_to_any_true(array)
+  i = 0
+  while i < array.length do
+    return true if array[i]
+    i += 1
+  end
+  return false
+end
