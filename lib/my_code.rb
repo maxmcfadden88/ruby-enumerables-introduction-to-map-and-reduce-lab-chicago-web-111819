bec53192551cfa61d4e708d83d0ee838864faf16
Max M <maxmcfadden88@gmail.com>
2019-11-16 01:03:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,3 +60,12 @@ def reduce_to_all_true(array)
   end
   return true
 end
+
+def reduce_to_any_true(array)
+  i = 0
+  while i < array.length do
+    return true if array[i]
+    i += 1
+  end
+  return false
+end
